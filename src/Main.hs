f2c16c2ea6d19677f07a237c08276d44a0a24ae5
Aditya Bhargava <bluemangroupie@gmail.com>
2014-04-14 17:53:52 -0700

    chapel strategy

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,32 @@
 import Dominion
 import Dominion.Cards
 import Dominion.Strategies
+import Control.Monad hiding (join)
+import qualified Dominion.Types as T
+import Control.Monad.IO.Class
 
-main = dominionWithOpts [Cards [market, smithy], Iterations 1, Log True] ["adit" `uses` bigMoney, "maggie" `uses` bigMoney]
+
+chapelStrategy p = do
+    chapels <- countNum p chapel
+    money <- handValue p
+    currentRound <- getRound
+
+    -- buy a chapel to start
+    when (chapels == 0 && money <= 3) $ do
+      p `buys` chapel
+      return ()
+
+    p `plays` laboratory
+    p `plays` councilRoom
+
+    -- if you have a chapel, use it
+    hasChapel <- p `has` chapel
+    when (hasChapel && money < 6) $ do
+      p `plays` chapel `with` (T.Chapel $ (4 `cardsOf` estate) ++ (2 `cardsOf` copper))
+      return ()
+
+    if currentRound < 8
+      then p `buysByPreference` [province, gold, laboratory, councilRoom, silver]
+      else p `buysByPreference` [province, gold, duchy, silver, estate]
+
+main = dominionWithOpts [Cards [chapel, gardens, councilRoom], Iterations 1000] ["adit" `uses` chapelStrategy, "maggie" `uses` bigMoney]
